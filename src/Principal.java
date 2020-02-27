459026adc13179d60f3bfcd45bc41ff5d455155b
elrichardev <47152450+elrichardev@users.noreply.github.com>
2020-02-27 09:14:42 +0100

    He añadido linea

--- src/Principal.java
+++ src/Principal.java
@@ -3,5 +3,6 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hola nacho");
+System.out.println("hola ricardo");
 	}
 }
