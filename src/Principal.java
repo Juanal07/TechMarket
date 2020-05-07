2b59bef6f9a2514265e76cb8bf8af7a0ea102f66
Juan Alberto Raya <jjrr1307@gmail.com>
2020-05-08 01:07:20 +0200

    login y opciones funcional

--- src/Principal.java
+++ src/Principal.java
@@ -6,46 +6,103 @@ import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		List<Persona> personas = GsonArray.desserializarJsonAArrayPersona();
-		List<Registro> registros = GsonArray.desserializarJsonAArrayRegistro();
+        List<Persona> personas = GsonArray.desserializarJsonAArrayPersona();
+        List<Registro> registros = GsonArray.desserializarJsonAArrayRegistro();
 
-		for (Registro r: registros) {
-			System.out.println(r.toString());
+//        for (Registro r : registros) {
+//            System.out.println(r.toString());
+//
+//        }
 
-		}
+        System.out.println("Bienvenido/a a TechMarket, introduzca su usuario para iniciar sesion");
 
-		System.out.println("Bienvenido/a a TechMarket, introduzca su usuario para iniciar sesion");
+        Scanner entrada = new Scanner(System.in);
+        String u = entrada.next();
 
-		Scanner entrada=new Scanner(System.in);
-		String u =entrada.next();
+        System.out.println("Introduzca su password");
 
-		System.out.println("Introduzca su password");
+        Scanner entrada2 = new Scanner(System.in);
+        String psw = entrada2.next();
 
-		Scanner entrada2=new Scanner(System.in);
-		String psw =entrada2.next();
+        boolean encontrado = false;
+        int i = 0;
+        while (i < personas.size() && !encontrado) {
+            if (personas.get(i).getUser().equals(u) && personas.get(i).getPassword().equals(psw)) {
+                encontrado = true;
+                System.out.println("Bienvenido " + personas.get(i).getUser());
+                if (personas.get(i).getTipo().equals("user"))
+                    opcionesUser();
+                else
+                    opcionesPowerUser();
+            }
+            i++;
+        }
+        if (!encontrado)
+            System.out.println("Usuario o password incorrecto");
+    }
 
-		for (Persona p: personas) {
-			if (p.getUser().equals(u)) {
-				if (p.getPassword().equals(psw)) {
-					System.out.println("Beinvenido "+p.getUser());
-				}else{
-					System.out.println("Contraseña incorrecta");
-				}
-			}else{
-				System.out.println("Usuario no existe");
-			}
+    public static void opcionesUser() {
 
-			
-		}
+        String opcion = "";
+        while (!opcion.equals("s")) {
+            System.out.println("Elija una opcion:" +
+                    '\n' + "-Gestionar registros(g)" +
+                    '\n' + "-Visualizar registros dados unos parametros de busqueda(v)" +
+                    '\n' + "-Salir(s)");
+            Scanner entrada3 = new Scanner(System.in);
+            opcion = entrada3.next();
+            switch (opcion) {
+                case "g":
+                    System.out.println("escogio la g");
+                    break;
+                case "v":
+                    System.out.println("escogio la v");
+                    break;
+                case "s":
+                    System.out.println("Hasta pronto");
+                    break;
+                default:
+                    System.out.println("error");
+                    break;
+            }
+        }
+    }
 
+    public static void opcionesPowerUser() {
+        String opcion = "";
+        while (!opcion.equals("s")) {
+            System.out.println("-Ver estadisticas(v)" +
+                    '\n' + "-Exportar registros a un csv(e)" +
+                    '\n' + "-Importar registros de un csv(i)" +
+                    '\n' + "-Enviar registros en csv por mail(m)" +
+                    '\n' + "-Salir(s)");
 
+            Scanner entrada4 = new Scanner(System.in);
+            opcion = entrada4.next();
 
+            switch (opcion) {
+                case "v":
+                    System.out.println("escogio la v");
+                    break;
+                case "e":
+                    System.out.println("escogio la e");
+                    break;
+                case "i":
+                    System.out.println("escogio la i");
+                    break;
+                case "m":
+                    System.out.println("escogio la m");
+                    break;
+                case "s":
+                    System.out.println("Hasta pronto");
+                    break;
+                default:
+                    System.out.println("error");
+                    break;
+            }
 
-
-
-
-
-	}
+        }
+    }
 }
