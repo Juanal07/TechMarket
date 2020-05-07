6919c1e3db7d3ea734fc78ef00f72951e2a65523
Juan Alberto Raya <jjrr1307@gmail.com>
2020-05-07 15:02:23 +0200

    hace login

--- src/Principal.java
+++ src/Principal.java
@@ -24,12 +24,24 @@ public class Principal {
 		System.out.println("Bienvenido/a a TechMarket, introduzca su usuario para iniciar sesion");
 
 		Scanner entrada=new Scanner(System.in);
+		String u =entrada.next();
 
-		String user =entrada.next();
+		System.out.println("Introduzca su password");
+
+		Scanner entrada2=new Scanner(System.in);
+		String psw =entrada2.next();
 
 		for (Persona p: personas) {
-			if (p.getUser().equals(entrada))
-				System.out.println("bien");
+			if (p.getUser().equals(u)) {
+				if (p.getPassword().equals(psw)) {
+					System.out.println("Beinvenido "+p.getUser());
+				}else{
+					System.out.println("Contraseña incorrecta");
+				}
+			}else{
+				System.out.println("Usuario no existe");
+			}
+
 			
 		}
 
