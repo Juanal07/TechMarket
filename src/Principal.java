441f1bc4968fcd07c07c027adc042dbefc366437
elrichardev <47152450+elrichardev@users.noreply.github.com>
2020-05-09 17:38:54 +0200

    He tocado un poco el código de la clase Principal. Corregido pequeños errores sin importancia. Añadido pequeños cambios estéticos.

--- src/Principal.java
+++ src/Principal.java
@@ -5,101 +5,172 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
-	
+
 	static Scanner entrada = new Scanner(System.in);
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        List<Persona> personas = GsonArray.desserializarJsonAArrayPersona();
-        List<Registro> registros = GsonArray.desserializarJsonAArrayRegistro();
+		//mensaje de entrada
+		System.out.println("");
+		System.out.println("・ーーーーーーーーーーーーーーーーーーー・");
+		System.out.println("《                     TECHMARKET        》");
+		System.out.println("・ーーーーーーーーーーーーーーーーーーー・");   
 
-        for (Registro r : registros) {
-            System.out.println(r.toString());
+		login();
 
-        }
 
-        System.out.println("Bienvenido/a a TechMarket, introduzca su usuario para iniciar sesion");        
-        String u = entrada.next();
-        System.out.println("Introduzca su password");
-        String psw = entrada.next();
 
-        boolean encontrado = false;
-        int i = 0;
-        while (i < personas.size() && !encontrado) {
-            if (personas.get(i).getUser().equals(u) && personas.get(i).getPassword().equals(psw)) {
-                encontrado = true;
-                System.out.println("Bienvenido " + personas.get(i).getUser());
-                if (personas.get(i).getTipo().equals("user"))
-                    opcionesUser();
-                else
-                    opcionesPowerUser();
-            }
-            i++;
-        }
-        if (!encontrado)
-            System.out.println("Usuario o password incorrecto");
-    entrada.close();}
+	}
 
-    public static void opcionesUser() {
+	public static void login() {
 
-        String opcion = "";
-        while (!opcion.equals("s")) {
-            System.out.println("Elija una opcion:" +
-                    '\n' + "-Gestionar registros(g)" +
-                    '\n' + "-Visualizar registros dados unos parametros de busqueda(v)" +
-                    '\n' + "-Salir(s)");
-            opcion = entrada.next();
-            switch (opcion) {
-                case "g":
-                    System.out.println("escogio la g");
-                    break;
-                case "v":
-                    System.out.println("escogio la v");
-                    break;
-                case "s":
-                    System.out.println("Hasta pronto");
-                    break;
-                default:
-                    System.out.println("error");
-                    break;
-            }
-            
-        }
-        
-    }
+		List<Persona> personas = GsonArray.desserializarJsonAArrayPersona();
+		List<Registro> registros = GsonArray.desserializarJsonAArrayRegistro();
 
-    public static void opcionesPowerUser() {
-        String opcion = "";
-        while (!opcion.equals("s")) {
-            System.out.println("-Ver estadisticas(v)" +
-                    '\n' + "-Exportar registros a un csv(e)" +
-                    '\n' + "-Importar registros de un csv(i)" +
-                    '\n' + "-Enviar registros en csv por mail(m)" +
-                    '\n' + "-Salir(s)");
+		//for (Registro r : registros) {
+			//System.out.println(r.toString());
+		//}
 
-            opcion = entrada.next();
+		System.out.println("Introduce tu nombre de usuario: ");
+		String u = entrada.next();
+		System.out.println(" ");
 
-            switch (opcion) {
-                case "v":
-                    System.out.println("escogio la v");
-                    break;
-                case "e":
-                    System.out.println("escogio la e");
-                    break;
-                case "i":
-                    System.out.println("escogio la i");
-                    break;
-                case "m":
-                    System.out.println("escogio la m");
-                    break;
-                case "s":
-                    System.out.println("Hasta pronto");
-                    break;
-                default:
-                    System.out.println("error");
-                    break;
-            }
 
-        }
-    }
+		System.out.println("Introduce tu contraseña:  ");
+		String psw = entrada.next();
+		System.out.println(" ");
+
+
+		boolean encontrado = false;
+		int i = 0;
+		while (i < personas.size() && !encontrado) {
+			if (personas.get(i).getUser().equals(u) && personas.get(i).getPassword().equals(psw)) {
+				encontrado = true;
+				System.out.println("\n \n¡Bienvenido de nuevo " + personas.get(i).getUser() + "!") ;
+				if (personas.get(i).getTipo().equals("user"))
+					opcionesUser();
+				else
+					opcionesPowerUser();
+			}
+			i++;
+		}
+		if (!encontrado)
+			System.out.println("El usuario o contraseña es incorrecto.");
+		    System.out.println("¡Vuelve a intentarlo de nuevo!\n \n");
+		login();
+
+		
+
+	}
+
+	public static void opcionesUser() {
+
+		String opcion = "";
+		while (!opcion.equals("s")) {
+
+			System.out.println("╔═══════════════════════════════════╗");
+			System.out.println("║        ¿Qué desea realizar?       ║");
+			System.out.println("║                                   ║");
+			System.out.println("║ ● GESTIONAR REGISTRO   (Pulse G)  ║");
+			System.out.println("║ ● VISUALIZAR REGISTRO  (Pulse V)  ║");
+			System.out.println("║ ● SALIR                (Pulse S)  ║");
+			System.out.println("╚═══════════════════════════════════╝");
+
+			opcion = entrada.next();
+			switch (opcion) {
+			case "g":
+				System.out.println("Has escogido [GESTIONAR REGISTRO]");
+				break;
+			case "v":
+				System.out.println("Has escogido [VISUALIZAR REGISTRO]");
+				break;
+			case "s":
+				System.out.println("Has escogido [SALIR]");
+				System.out.println("¡Hasta pronto!");
+				break;
+			default:
+				System.out.println("¡Ha surgido un error, vuelve a intentarlo!");
+				break;
+			}
+
+		}
+
+	}
+
+	public static void opcionesPowerUser() {
+		String opcion = "";
+		while (!opcion.equals("s")) {
+
+			System.out.println("╔═══════════════════════════════════════════════╗");
+			System.out.println("║              ¿Qué desea realizar?             ║");
+			System.out.println("║                                               ║");
+			System.out.println("║ ● VISUALIZAR ESTADÍSTICAS          (Pulse V)  ║");
+			System.out.println("║ ● IMPORTAR REGISTROS .CSV          (Pulse I)  ║");
+			System.out.println("║ ● EXPORTAR REGISTROS .CSV          (Pulse E)  ║");
+			System.out.println("║ ● ENVIAR REGISTROS .CSV POR EMAIL  (Pulse M)  ║");
+			System.out.println("║ ● SALIR                            (Pulse S)  ║");
+			System.out.println("╚═══════════════════════════════════════════════╝");
+
+			opcion = entrada.next();
+
+			switch (opcion) {
+			case "v":
+				System.out.println("Has escogido [VISUALIZAR ESTADÍSTICAS]");
+
+				//llamamos método
+				
+				break;
+			case "e":
+				System.out.println("Has escogido [EXPORTAR REGISTROS .CSV]");
+
+				exportarCsv();
+				
+				break;
+			case "i":
+				System.out.println("Has escogido [IMPORTAR REGISTROS .CSV]");
+
+				importarCsv();
+				
+				break;
+			case "m":
+				System.out.println("Has escogido [ENVIAR REGISTROS .CSV POR EMAIL]");
+
+				enviarCsv();
+				
+				break;
+			case "s":
+				System.out.println("Has escogido [SALIR]");
+				System.out.println("¡Hasta pronto!");
+				
+				break;
+			default:
+				System.out.println("Ha surgido un error, vuelve a intentarlo!");
+				break;
+			}
+
+		}
+	}
+	
+	public static void importarCsv() {
+		
+		//a completar
+		
+	}
+	
+	
+	public static void exportarCsv() {
+		
+		//a completar
+		
+	}
+	
+	
+	public static void enviarCsv() {
+		
+		//a completar
+		
+	}
+	
+	
+	
 }
